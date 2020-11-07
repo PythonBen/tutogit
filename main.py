eed30723dd484ecc4f51b7e1cddfefb9d8a0ecf8
PythonBen <benjamin.lepers@gmail.com>
2020-11-07 23:18:19 +0100

    'adding a class Person'

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """ testing git and pycharm"""
+class Person:
+    def __init__(self, name, age):
+        self.name, self.age = name, age
+    def __repr__(self):
+        return f'Person({self.name},{self.age})'
+
+
 if __name__ == '__main__':
     print('hello version control')
+    p = Person('bob', 20)
+    print(p)
